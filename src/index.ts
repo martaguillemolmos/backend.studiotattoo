d84b57f9154a9aa48cd45aafa9384d3ad19c4ce3
martaguillemolmos <martetaguillem@gmail.com>
2023-10-23 18:30:16 +0200

    feat: We create routes on the index.ts page for users

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import express from "express";
 
-const PORT = process.env.PORT || 4000
-const app = express()
-app.listen( PORT, ()=> {
-    console.log(`Server running ${PORT}`)
-})
+const PORT = process.env.PORT || 4000;
+const app = express();
+app.listen(PORT, () => {
+  console.log(`Server running ${PORT}`);
+});
 
+// Cremos rutas para usuarios
+app.get("/user", (req, res) => {
+  // lógica de la infor que recuperamos de los usuarios
+  return res.send("GET USUARIO");
+});
+
+app.post("/user", (req, res) => {
+  //lógica para crear usuarios
+  return res.send("CREAR USUARIO");
+});
+
+app.put("/user", (req, res) => {
+  //lógica para actualizar usuarios
+  return res.send("ACTUALIZAR USUARIO");
+});
+
+app.delete("/user", (req, res) => {
+  //lógica para eliminar usuarios
+  return res.send("ELIMINAR USUARIO");
+});

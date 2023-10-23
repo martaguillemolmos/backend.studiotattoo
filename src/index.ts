8bac747db1e439b898aaa3317f07b555595229a9
martaguillemolmos <martetaguillem@gmail.com>
2023-10-23 19:03:24 +0200

    perf: We transfer the get type route to useroutes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { router as routerUser} from "./routes/usersRoutes"
 
 const PORT = process.env.PORT || 4000;
+
 const app = express();
+app.use  (express.json())
+
+app.use('/user', routerUser)
+
 app.listen(PORT, () => {
   console.log(`Server running ${PORT}`);
 });
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
 
 const router = Router ()
+
+// Cremos rutas para usuarios
+router.get("/", (req, res) => {
+    // lógica de la infor que recuperamos de los usuarios
+    return res.send("GET USUARIO");
+  });
+
+export{router}

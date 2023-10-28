840987581014cd9ae4379d61f93a32e0d0045d25
martaguillemolmos <martetaguillem@gmail.com>
2023-10-28 15:32:01 +0200

    fix: Create migration table user

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,17 @@
-import "reflect-metadata"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+const AppDataSource = new DataSource({
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "root",
+  password: "1234",
+  database: "studio_tatto",
+  entities: [],
+  migrations: [],
+  synchronize: false,
+  logging: false,
+});
+
+export { AppDataSource };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { router as routerUser} from "./routes/usersRoutes"
+import { AppDataSource } from "./db";
 
 const PORT = process.env.PORT || 4000;
 
@@ -7,6 +8,8 @@ const app = express();
 app.use  (express.json())
 
 app.use('/user', routerUser)
+
+AppDataSource
 
 app.listen(PORT, () => {
   console.log(`Server running ${PORT}`);

ec195b2759685a1d38d8fe0acb0baa034995b28d
martaguillemolmos <martetaguillem@gmail.com>
2023-10-28 18:50:49 +0200

    feat: Downland bcypt and types/bcrypt.

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -7,12 +7,17 @@ const router = Router ()
 
 //Acceder a todos los usuarios registrados
 router.get("/", getUser);
+
 //Crear un usuario
 router.post("/", createUser);
+
 //Actualizar un usuario por el Id
 router.put("/:id", updateUserById);
+
 //Eliminar un usuario por el Id
 router.delete("/:id", deleteUserbyId);
+
+//Login
 
 export {router}
 

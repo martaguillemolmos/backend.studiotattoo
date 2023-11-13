2724e3553c5ac12f97d34c2bb35f49a1372e0b85
martaguillemolmos <martetaguillem@gmail.com>
2023-11-13 16:49:18 +0100

    feat: Modified the way of declaring cors in index.ts.

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use (express.json());
-app.use (cors);
+app.use (cors());
 
 app.use('/user', routerUser)
 app.use ('/worker', routerWorker)
